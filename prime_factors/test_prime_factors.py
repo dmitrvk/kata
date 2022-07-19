cd41e458e208bebff24dc2be5ca9e989509c443c
Dmitry Kalyukov <dmitrykalyukov@gmail.com>
2022-07-19 21:19:20 +0200

    Use plain dict for test cases

--- prime_factors/test_prime_factors.py
+++ prime_factors/test_prime_factors.py
@@ -1,22 +1,18 @@
-from collections import namedtuple
 from typing import Final
 
 from prime_factors import PrimeFactors
 
 
 class TestPrimeFactors:
-    Case = namedtuple('Case', ('number', 'factors'))
-
-    CASES: Final[tuple[Case, ...]] = (
-        Case(1, []),
-        Case(2, [2]),
-        Case(4, [2, 2]),
-        Case(4, [2, 2]),
-        Case(8, [2, 2, 2]),
-        Case(9, [3, 3]),
-    )
+    CASES: Final[dict[int, list[int]]] = {
+        1: [],
+        2: [2],
+        4: [2, 2],
+        4: [2, 2],
+        8: [2, 2, 2],
+        9: [3, 3],
+    }
 
     def test_prime_factors(self) -> None:
-        for test_case in self.CASES:
-            factors = PrimeFactors.calculate(test_case.number)
-            assert factors == test_case.factors
+        for number, expected_factors in self.CASES.items():
+            assert PrimeFactors.calculate(number) == expected_factors
